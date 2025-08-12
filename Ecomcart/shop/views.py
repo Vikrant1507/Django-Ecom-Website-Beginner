8864523f2ea860643db579a6cd3e3955bfb47191
Vikrant Singh Guleria <vikrantsinghguleria1999@gmail.com>
2025-08-12 11:37:03 +0530

    Fourth Commit

--- Ecomcart/shop/views.py
+++ Ecomcart/shop/views.py
@@ -1,39 +1,61 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import product
-import  math
+from math import ceil
+
 
 # Create your views here.
 def index(request):
     products = product.objects.all()
-    n= len(products)
-    nslides = n//4 + math.ceil((n/4) + (n//4))
-    params = {'no_of_slides': nslides, 'range': range(1, nslides), 'product': products}
-    return render(request, 'shop/index.html',params)
+    print(products)
+    n = len(products)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+
+    # allProds = []
+    # catprods = product.objects.values('category', 'id')
+    # cats = {item['category'] for item in catprods}
+    # for cat in cats:
+    #     prod = product.objects.filter(category=cat)
+    #     n = len(prod)
+    #     nSlides = n // 4 + ceil((n / 4) - (n // 4))
+    #     allProds.append([prod, range(1, nSlides), nSlides])
+
+    params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    allProds = [[products, range(1, nSlides), nSlides],
+                [products, range(1, nSlides), nSlides]]
+    params = {'allProds': allProds}
+    return render(request, 'shop/index.html', params)
+
 
 # def index(request):
 #     products = product.objects.all()
 #     return render(request, 'shop/index.html', {'products': products})
 
 def product_list(request):
-    products = product.objects.all() # Retrieves all product objects
+    products = product.objects.all()  # Retrieves all product objects
     context = {'products': products}
     return render(request, 'shop/product_list.html', context)
+
 
 def about(request):
     return render(request, 'shop/about.html')
 
+
 def contact(request):
     return HttpResponse('Contact Us')
+
 
 def tracker(request):
     return HttpResponse('Tracker')
 
+
 def Search(request):
     return HttpResponse('Search')
 
+
 def ProductView(request):
     return HttpResponse('ProductView')
+
 
 def checkout(request):
     return HttpResponse('Checkout')
