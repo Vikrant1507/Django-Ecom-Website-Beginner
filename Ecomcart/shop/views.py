e8e79fece425d4947a09fe98a1fa9ce407e8bc4e
Vikrant Singh Guleria <vikrantsinghguleria1999@gmail.com>
2025-08-13 09:34:01 +0530

    Fifth Commit

--- Ecomcart/shop/views.py
+++ Ecomcart/shop/views.py
@@ -7,28 +7,24 @@ from math import ceil
 # Create your views here.
 def index(request):
     products = product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
-
-    # allProds = []
-    # catprods = product.objects.values('category', 'id')
-    # cats = {item['category'] for item in catprods}
-    # for cat in cats:
-    #     prod = product.objects.filter(category=cat)
-    #     n = len(prod)
-    #     nSlides = n // 4 + ceil((n / 4) - (n // 4))
-    #     allProds.append([prod, range(1, nSlides), nSlides])
-
-    params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
-    allProds = [[products, range(1, nSlides), nSlides],
-                [products, range(1, nSlides), nSlides]]
+    allProds = []
+    catprods = product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
     params = {'allProds': allProds}
     return render(request, 'shop/index.html', params)
 
 
 # def index(request):
 #     products = product.objects.all()
+#     n = len(products)
+#     nSlides = n // 4 + ceil((n / 4) - (n // 4))
+#     allProds = [[products, range(1, nSlides), nSlides],
+#             [products, range(1, nSlides), nSlides]]
 #     return render(request, 'shop/index.html', {'products': products})
 
 def product_list(request):
